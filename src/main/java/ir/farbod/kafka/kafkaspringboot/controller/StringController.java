7f72024d2b5b992325d3c8d9d61c6c4cd9587c74
farbod2u <saeedsaf@gmail.com>
2021-12-11 14:01:03 +0330

    Initial commit

---
+++ src/main/java/ir/farbod/kafka/kafkaspringboot/controller/StringController.java
@@ -0,0 +1,23 @@
+package ir.farbod.kafka.kafkaspringboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+@RequiredArgsConstructor
+public class StringController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    private static final String TOPIC = "KafkaSample";
+
+    @GetMapping("/publish/{message}")
+    public String publish(@PathVariable String message) {
+        kafkaTemplate.send(TOPIC, message);
+        return "Successfully publish Message ==> " + message;
+    }
+}
